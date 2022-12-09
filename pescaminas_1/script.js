33ea5bb10b3634be882c0b6b41c20f3f75e8f339
andrescarles <acarles@jaumebalmes.net>
2022-12-09 09:58:13 +0100

    final

--- pescaminas_1/script.js
+++ pescaminas_1/script.js
@@ -1,8 +1,25 @@
+//VARIABLES GLOBALES (usadas en la primera función)
+let x; //almacena longitud x
+let y; //almacena longitud y
+let min; //almacena numero de minas en el juego
+let tablero; //almacena array binario 
+
+//LOCALSTORAGE PARA RECOGER PUNTUACIONES
+if (localStorage.getItem("puntuacion") != null) {
+    document.getElementById('ul').insertAdjacentHTML('beforeend', localStorage.getItem("puntuacion"));
+}
+
+
+//FUNCION PARA CREAR LA TABLA E INICIALIZAR VALORES
 function inicialitzaJoc() {
-    //INPUTS
-    let x = parseInt(document.getElementById("x").value);
-    let y = parseInt(document.getElementById("y").value);
-    //BUCLES PARA LLENAR FILAS Y COLUMNAS
+    //LLENAR INPUTS
+    let a = parseInt(document.getElementById("x").value);
+    let b = parseInt(document.getElementById("y").value);
+    min = document.getElementById("numeroMinas").value;
+    x = a;
+    y = b;
+    tablero = inicialitzaMines(min, x, y);
+    //BUCLES PARA CREAR FILAS Y COLUMNAS
     for (let i = 0; i < x; i++) {
         let filaActual = document.getElementById('tabla').insertRow(i);
         for (let j = 0; j < y; j++) {
@@ -11,9 +28,15 @@ function inicialitzaJoc() {
             celda.setAttribute("id", "X:" + i + " " + "Y:" + j);
         }
     }
+    //OCULTAR BOTONES DEL HTML
+    document.getElementById("boton").style.display = "none";
+    document.getElementById("x").style.display = "none";
+    document.getElementById("y").style.display = "none";
+    document.getElementById("numeroMinas").style.display = "none";
+    document.getElementById("crono").style.display = "inline";
 }
 
-//FUNCION QUE SELECCIONA LA TABLA PINTADA EN EL HTML
+//FUNCION QUE SELECCIONA LA TABLA FISICA DEL HTML
 function selectorTabla() {
     let rows = document.querySelector("tbody").children
     let matrix = []
@@ -23,23 +46,9 @@ function selectorTabla() {
     return matrix;
 }
 
-//FUNCION BINARIZAR ARRAY
-function matriuBinaria(matrix) {
-    let matrix2 = matrix;
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            if (matrix[i][j].style.backgroundColor == "red") {
-                matrix2[i][j].innerHTML = "1";
-            } else {
-                matrix2[i][j].innerHTML = "0";
-            }
-        }
-    }
-    return matrix2;
-}
-//FUNCION INICIAR MINAS
+//FUNCION PARA CREAR TABLA BINARIA CON LAS MINAS PUESTAS ALEATORIAMENTE
 function inicialitzaMines(nminas, x, y) {
-    //VARIABLE GLOBAL MINAS
+    //ARRAY BINARIO
     let minas = new Array(x);
     for (let i = 0; i < x; i++) {
         minas[i] = new Array(y);
@@ -68,57 +77,38 @@ function inicialitzaMines(nminas, x, y) {
     return minas;
 }
 
-//FUNCION PINTAR ARRAYS BINARIOS
-function pintaMinas(matrix) {
-    //TABLA Y MATRIX TIENEN LA MISMA LONGITUD YA QUE AMBOS RECIBEN COMO PARAMETRO LOS INPUTS DEL HTML
-    tabla = selectorTabla();
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            if (matrix[i][j] == 1) {
-                tabla[i][j].style.backgroundColor = "red";
-            } else {
-                tabla[i][j].style.backgroundColor = "white";
-            }
-        }
-    }
-    return tabla;
-}
-//FUNCION PARA VER EL RESULTADO DE LAS MINAS INICIADAS Y PINTADAS
-function iniciarMinas() {
-    //INPUTS
-    let x = parseInt(document.getElementById("x").value);
-    let y = parseInt(document.getElementById("y").value);
-    let minasIniciadas = inicialitzaMines(5, x, y);
-    pintaMinas(minasIniciadas);
-
-}
-
 //FUNCION ONCLICK
-
 document.addEventListener("click", (e) => {
     if (e.target.tagName == 'TD') {
         console.log(e.target.id)
         let regex = /(\d+)/g;
+        //EXTRAEMOS LAS COORDENADAS A PARTIR DEL ID
         let string = e.target.id.split(' ');
-        let x = parseInt(string[0].match(regex));
-        let y = parseInt(string[1].match(regex));
-        buscaminas(x, y);
+        let a = parseInt(string[0].match(regex));
+        let b = parseInt(string[1].match(regex));
+        //ENTRAMOS EN LA FUNCION QUE EVALUARÁ EL CLICK
+        buscaminas(a, b);
     }
 })
 
-// NUMEROS BUSCAMINAS //
+//FUNCION PARA PINTAR EN VERDE UNA CASILLA CON CLICK DERECHO
+document.addEventListener("contextmenu", (e) => {
+    if (e.target.tagName == 'TD') {
+        e.target.style.backgroundColor = "green";
+    }
+});
 
+//FUNCION QUE CUENTA EL NUMERO DE MINAS ALREDEDOR
 function contador(x, y) {
-    matrix = selectorTabla();
     let contador = 0;
     for (let i = x - 1; i < x + 2; i++) {
         for (let j = y - 1; j < y + 2; j++) {
             try {
                 if (i == x - 1 || i == x + 1) {
-                    if (matrix[i][j].style.backgroundColor == "red") { contador++; }
+                    if (tablero[i][j] == 1) { contador++; }
                 } else {
                     if (j == y + 1 || j == y - 1) {
-                        if (matrix[i][j].style.backgroundColor == "red") { contador++; }
+                        if (tablero[i][j] == 1) { contador++; }
                     }
                 }
             } catch (error) {}
@@ -127,7 +117,117 @@ function contador(x, y) {
     return contador;
 }
 
-function buscaminas(x, y) {
+//FUNCION QUE MIRA LOS ALREDEDORES DE UNA CASILLA
+function buscaespacios(x, y) {
     matrix = selectorTabla();
-    matrix[x][y].innerHTML = "<center>" + contador(x, y) + "</center>";
+    for (let i = x - 1; i < x + 2; i++) {
+        for (let j = y - 1; j < y + 2; j++) {
+            try {
+                if (i == x - 1 || i == x + 1) {
+                    matrix[i][j].innerHTML = contador(i, j);
+                } else {
+                    if (j == y + 1 || j == y - 1) {
+                        matrix[i][j].innerHTML = contador(i, j);
+                    }
+                }
+            } catch (error) {}
+        }
+    }
+}
+
+//FUNCIÓN QUE EVALUA LA LÓGICA DEL JUEGO
+function buscaminas(x, y) {
+    let fin = true;
+    matrix = selectorTabla();
+    if (tablero[x][y] == 1) { //SI PISAS UNA MINA ...
+        parar();
+        document.getElementsByTagName("table")[0].style.pointerEvents = "none";
+        document.getElementsByTagName("table")[0].style.backgroundColor = "grey";
+        document.getElementById("lost").style.display = "block";
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[0].length; j++) {
+                matrix[i][j].style.backgroundColor = "grey";
+            }
+        }
+        matrix[x][y].style.backgroundColor = "red";
+        return ""
+    }
+    buscaespacios(x, y); //EVALUAMOS LOS ALREDEDORES DE LA CASILLA PINCHADA
+    matrix[x][y].innerHTML = contador(x, y); // EVALUAMOS LA CASILLA PINCHADA
+
+    //EVALUAMOS SI EL JUEGO DEBERIA TERMINAR MIRANDO LAS CASILLAS DESTAPADAS
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j].innerHTML == '&nbsp;&nbsp;&nbsp;') {
+                fin = false; //SI QUEDAN CASILLAS POR DESTAPAR EL FIN DEL JUEGO ES FALSO
+                break;
+            }
+        }
+    }
+    if (fin) { //SI NO QUEDAN CASILLAS POR DESTAPAR FIN
+        parar(); //SE PARA EL CRONOMETRO
+        document.getElementsByTagName("table")[0].style.pointerEvents = "none";
+        document.getElementsByTagName("table")[0].style.backgroundColor = "green";
+        document.getElementById("win").style.display = "block";
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[0].length; j++) {
+                matrix[i][j].style.backgroundColor = "green";
+            }
+        }
+        puntuaciones(); //LLAMAMOS FUNCION QUE SE ENCARGA DE REGISTRAR PUNTUACIONES
+
+    }
+}
+
+//CRONOMETRO
+var h1 = document.getElementsByTagName('h1')[0];
+var sec = 0;
+var minutos = 0;
+var hrs = 0;
+var t;
+
+function tick() {
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        minutos++;
+        if (minutos >= 60) {
+            minutos = 0;
+            hrs++;
+        }
+    }
+}
+
+function add() {
+    tick();
+    h1.textContent = (hrs > 9 ? hrs : "0" + hrs) +
+        ":" + (minutos > 9 ? minutos : "0" + minutos) +
+        ":" + (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+
+function timer() {
+    t = setTimeout(add, 1000);
+}
+let parar = function() {
+    clearTimeout(t);
+}
+
+document.getElementById('tabla').addEventListener("click", timer, { once: true }) //EL CRONOMETRO EMPIEZA CON EL PRIMER CLICK EN UNA CELDA
+
+//PUNTUACIONES
+function panel() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(prompt("Introduce tu nombre"));
+        });
+    });
+}
+async function puntuaciones() {
+    let person = await panel();
+    let ul = document.getElementById("ul");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(person + " : " + document.getElementById("crono").textContent));
+    ul.appendChild(li);
+    localStorage.setItem("puntuacion", document.getElementById("ul").innerHTML)
 }
